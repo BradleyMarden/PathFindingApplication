94bfad3533e5075cbacbcecf99a4cae7264366a9
BradleyMarden <bradleymarden@ez-gamelogic.com>
2020-08-04 01:13:44 +0100

    Init

---
+++ cmake-build-debug/CMakeFiles/PathFinding.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PathFinding.dir/src/main.cpp.obj"
+  "PathFinding.exe"
+  "PathFinding.exe.manifest"
+  "PathFinding.pdb"
+  "libPathFinding.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PathFinding.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
